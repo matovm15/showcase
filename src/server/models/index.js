8bab8826b3962c9964c5e39408b1de7008f43801
kallyas <kallyasmedia@gmail.com>
2022-10-20 21:48:45 +0300

    fix: model errors

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -1,8 +1,13 @@
 import User from "./user.model.js";
 import Token from "./token.model.js";
+import Service from "./service.model.js";
+import Gig from "./gig.model.js";
+import Profile from "./profile.model.js";
 
 
 export {
     User,
-    Token
+    Token,
+    Service,
+    Gig
 }
