c35a34c6ceed2aedd08ee96a3280f091f6c40893
kallyas <kallyasmedia@gmail.com>
2022-10-22 19:14:59 +0300

    ch: add profile routes

---
+++ src/server/routes/v1/profile.route.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import validate from '../../middlewares/validate.js';
+import profileValidation from '../../validations/profile.validation.js';
+import auth from '../../middlewares/auth.js';
+import { profileController } from '../../controllers/profile.controller.js';
+
+const router = Router();
+
+router
+  .route('/')
+  .post(auth('manageProfiles'), validate(profileValidation.createProfile), profileController.createProfile)
+  .get(auth('getProfiles'), validate(profileValidation.getProfiles), profileController.getProfiles);
+
+router
+  .route('/:profileId')
+  .get(auth('getProfiles'), validate(profileValidation.getProfile), profileController.getProfileById)
+  .patch(auth('manageProfiles'), validate(profileValidation.updateProfile), profileController.updateProfileById)
+  .delete(auth('manageProfiles'), validate(profileValidation.deleteProfile), profileController.deleteProfileById);
+
+export default router;
