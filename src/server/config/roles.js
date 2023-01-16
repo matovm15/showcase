7bf04b3bd96e15c8cd5602859e06b4481518439d
kallyas <kallyasmedia@gmail.com>
2023-01-16 21:33:07 +0300

    fix: roles

--- src/server/config/roles.js
+++ src/server/config/roles.js
@@ -1,8 +1,8 @@
 const allRoles = {
   user: ['getProfile', 'manageProfile', 'getUser'],
   admin: ['getUsers', 'manageUsers', 'getProfiles', 'manageProfiles'],
-  freelancer: ['getProfile', 'manageProfile', 'getUserViaToken'],
-  client: [],
+  freelancer: ['getProfile', 'manageProfile', 'getUserViaToken', 'getUser'],
+  client: ['getProfile', 'manageProfile', 'getUser'],
 };
 
 const roles = Object.keys(allRoles);
